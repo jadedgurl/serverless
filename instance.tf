d9e006a428d2a1876a02febe349faf0ecfe01868
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-05-03 19:05:20 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key=var.AWS_SECRET_KEY
 }
 resource "aws_instance" "web" {
-  ami = "ami-0889a44b331db0194"
+  ami = "ami-0da62eb5869c785b9"
   instance_type = "t3.micro"
   
   tags = {
